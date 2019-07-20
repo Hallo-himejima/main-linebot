a51a584cea09984a46ee6f2d33e2102a81788838
himejima keisuke <himehime.mehimehi.330@gmail.com>
2019-07-20 14:46:16 +0900

    直し

--- new-line-bot.py
+++ new-line-bot.py
@@ -49,7 +49,7 @@ def handle_message(event):
     username = profile.display_name
     user_message = event.message.text
 
-    image=[https://image.news.livedoor.com/newsimage/7/e/7eefb_1538_900d9e320d7ffc311caffadd222e6183.png,
+    image=[https://files.value-press.com/czMjYXJ0aWNsZSMyMjcwMSMxODk5MTkjMjI3MDFfRFp0aEdRQk1sSy5qcGc.jpg
     https://prtimes.jp/i/22436/5/origin/d22436-5-177370-pixta_29876635-1.jpg,
     https://realsound.jp/wp-content/uploads/2018/02/180222-cr-j-6.jpg
     ]
